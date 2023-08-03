daae858cd3928b9384bd291685c99d1a8b8d28bd
devAInsyte <dev@ainsyte.com>
2023-08-03 04:02:16 -0700

    Removed previous button and functionality, added ?qn=N url param to jump to specific question.

--- personality_inventory.js
+++ personality_inventory.js
@@ -1,6 +1,6 @@
 //Function to create object with inventory  of questions pulling from the hidden p elements inside the secion.gh-content element.
 //This allows the CMS to to provide the test data in the form of stylesheet hidden <p> tags in the content response.
-function extractQuestions() {
+function extractQuestions() { //#Is called to create the variable currentTest obj at the start of each test and is passed to populateQuestions on initialization.
 
   //Load document HTML and create a parser to pull the text/html from hidden p tags inside the section.gh-content block.
   const htmlString = document.documentElement.innerHTML;
@@ -16,33 +16,63 @@ function extractQuestions() {
 
   //Return the object of questions and answers.
   return pObject;
+}
 
+//Function to display question number / progress. #Called by populateQuestions().
+function updateQuestionNumberDisplay(){
+
+  //Get the question number user display element.
+  let questionNumberDisplay = document.getElementById("questionNumberDisplay"); //currentTest[index].split('|')[0];
+  //Set the question number user display to the currnet question number.
+  questionNumberDisplay.innerHTML = currentQuestionNumber;
+  
+  //Get the total number of questions user display element.
+  let testLengthDisplay = document.getElementById("testLengthDisplay");
+  //set the totla number of questions user display element to the number of questions in the currentTest obj. 
+  testLengthDisplay.innerHTML = Object.keys(currentTest).length;
+}
+
+//Function to clear checkboxes. #Called by populateQusetions();
+function clearCheckBoxes(){
+
+  //Grab the answer options objects.
+  let answers  = document.getElementsByClassName("answerOptions");
+
+  //loop through them...
+  for (let i = 0; i < answers.length; i++) {
+    //Checking to see if they are checked. 
+    if(answers[i].checked){ //If they are checked...
+      //...uncheck them (reset for next question.)
+      answers[i].checked = false;
+    }
+  }
 }
 
 //Function to populate initial question and answer options to div#currentQuestion, starting at question #1.
-function populateQuestions(testInput, index) {
+function populateQuestions(testInput, index) { //#Is called on initalization and moveToNextQuestion() 
 
 //## Debug logging ## Uncomment to see in console. 
-//  console.log(); //
 //    console.log("Question Number:" + index); //Write the current question numnber to the console.
 //    console.log("Total Questions in Current Test:" + Object.keys(testInput).length); //Write total number of questions found in the current test to the console.
 //    console.log("Current Question:" + testInput[index].split('|')[0]); //Write the current question text to the console.
 //    console.log("Current Answer Options:" + testInput[index].split('|')[1]); //Write the current answer options to the console.
 //    console.log(testInput); //Write the current tests object to the console.
 
-  //Populate question text, index 0 after splitting by pipe delineation.
-  document.getElementById("currentQuestion").innerHTML = testInput[index].split('|')[0];
+  //Get the current question user display element.
+  let questionText = document.getElementById("currentQuestion");
+  //Populate current question user display with current question text, index 0 after splitting by pipe delineation.
+  questionText.innerHTML = testInput[index].split('|')[0];
 
-  //Get the number of answers associated with the indexed quesiton to decide how many answer options to display.
+  //Get the number of answers associated with the indexed question to decide how many answer options to display.
   let numberOfAnswerOptions = testInput[index].split('|')[1].split(',').length;
 
   //Create an object of the answer check box options using the class name answerOptions. 
   let answerOptionBoxes = document.getElementsByClassName("answerOptions");
 
-  //create an object of the anser check box label options using the class name answerOptions-label.
+  //Create an object of the answer check box label options using the class name answerOptions-label.
   let answerOptionLabels = document.getElementsByClassName("answerOptions-label");
 
-  //grab div#answer-inputs wrapper to remove when text only input is desired.
+  //Grab div#answer-inputs wrapper to remove when text only input is desired.
   let answerInputs = document.getElementById("answer-inputs");
 
   //Toggle answer options based off the number available, must be at least one option, in which case it is assumed that the answer should be free-form text.
@@ -50,7 +80,7 @@ function populateQuestions(testInput, index) {
 
     //Providing a single answer option (cannot be 0 for now, will update) is equivalent to providing only free-form text as an answer.
     case 1:
-      //remove div.answerInputs so layout doesn't have white space when text only input is requested.
+      //Remove div.answerInputs so layout doesn't have white space when text only input is requested.
       answerInputs.style.display = "none";
       //Update checkboxes.
       answerOptionBoxes[0].value = "";
@@ -61,7 +91,7 @@ function populateQuestions(testInput, index) {
       answerOptionBoxes[1].style.display = "none";
       answerOptionBoxes[2].style.display = "none";
       answerOptionBoxes[3].style.display = "none";
-      //Update labels
+      //Update labels.
       answerOptionLabels[0].innerText = "";
       answerOptionLabels[1].innerText = "";
       answerOptionLabels[2].innerText = "";
@@ -74,7 +104,7 @@ function populateQuestions(testInput, index) {
 
     //Providing any other N<=4 will display each answer as a button and the free-form text box. 
     case 2:
-      //makes sure answerInputs div is displayed, will not be if coming from a text only input question.
+      //Make sure answerInputs div is displayed, will not be if coming from a text only input question.
       answerInputs.style.display = "block";
       //Update checkboxes.
       answerOptionBoxes[0].value = testInput[index].split('|')[1].split(',')[0];
@@ -85,7 +115,7 @@ function populateQuestions(testInput, index) {
       answerOptionBoxes[1].style.display = "inline";
       answerOptionBoxes[2].style.display = "none";
       answerOptionBoxes[3].style.display = "none";
-      //update labels
+      //Update labels.
       answerOptionLabels[0].innerText = testInput[index].split('|')[1].split(',')[0];
       answerOptionLabels[1].innerText = testInput[index].split('|')[1].split(',')[1];
       answerOptionLabels[2].innerText = "";
@@ -97,7 +127,7 @@ function populateQuestions(testInput, index) {
       break;
 
     case 3:
-      //makes sure answerInputs div is displayed, will not be if coming from a text only input question.
+      //Make sure answerInputs div is displayed, will not be if coming from a text only input question.
       answerInputs.style.display = "block";
       //Update checkboxes.
       answerOptionBoxes[0].value = testInput[index].split('|')[1].split(',')[0];
@@ -108,7 +138,7 @@ function populateQuestions(testInput, index) {
       answerOptionBoxes[1].style.display = "inline";
       answerOptionBoxes[2].style.display = "inline";
       answerOptionBoxes[3].style.display = "none";
-      //update labels
+      //update labels.
       answerOptionLabels[0].innerText = testInput[index].split('|')[1].split(',')[0];
       answerOptionLabels[1].innerText = testInput[index].split('|')[1].split(',')[1];
       answerOptionLabels[2].innerText = testInput[index].split('|')[1].split(',')[2];
@@ -120,7 +150,7 @@ function populateQuestions(testInput, index) {
       break;
 
     case 4:
-      //makes sure answerInputs div is displayed, will not be if coming from a text only input question.
+      //Make sure answerInputs div is displayed, will not be if coming from a text only input question.
       answerInputs.style.display = "block";
       //Update checkboxes.
       answerOptionBoxes[0].value = testInput[index].split('|')[1].split(',')[0];
@@ -144,43 +174,30 @@ function populateQuestions(testInput, index) {
 
     //Default behavior is to show no buttons and only free-form text input. This would only be in case of error.
     default:
-      //remove div.answerInputs so layout doesn't have white space when text only input is requested.
+      //Remove div.answerInputs so layout doesn't have white space when text only input is requested.
       answerInputs.style.display = "none";
       //Update checkboxes.
       answerOptionBoxes[0].value = "";
       answerOptionBoxes[1].value = "";
       answerOptionBoxes[2].value = "";
       answerOptionBoxes[3].value = "";
-      answerOptionBoxes[0].style.display = 'none';
-      answerOptionBoxes[1].style.display = 'none';
-      answerOptionBoxes[2].style.display = 'none';
-      answerOptionBoxes[3].style.display = 'none';
+      answerOptionBoxes[0].style.display = "none";
+      answerOptionBoxes[1].style.display = "none";
+      answerOptionBoxes[2].style.display = "none";
+      answerOptionBoxes[3].style.display = "none";
       //Update labels.
       answerOptionLabels[0].innerText = "";
       answerOptionLabels[1].innerText = "";
       answerOptionLabels[2].innerText = "";
       answerOptionLabels[3].innerText = "";
-      answerOptionLabels[0].style.display = 'none';
-      answerOptionLabels[1].style.display = 'none';
-      answerOptionLabels[2].style.display = 'none';
-      answerOptionLabels[3].style.display = 'none';
+      answerOptionLabels[0].style.display = "none";
+      answerOptionLabels[1].style.display = "none";
+      answerOptionLabels[2].style.display = "none";
+      answerOptionLabels[3].style.display = "none";
   }
 
   //Update question number and progress display.
   updateQuestionNumberDisplay();
-
-  //Address special case of the first question where you should not be able to move back by disabling the Previous button.
-  if(currentQuestionNumber == 1){
-
-    //If on the first question, disable the Previous button.
-    document.getElementById("previousQuestion").disabled = true;
-
-  }else{
-
-    //If not, ensure the previous button is enabled.
-    document.getElementById("previousQuestion").disabled = false;
-
-  }
 
   //Address special case of the last question where you should not be able to move forward. This will also be the end state trigger.
   if(currentQuestionNumber == Object.keys(testInput).length){
@@ -192,25 +209,13 @@ function populateQuestions(testInput, index) {
 
    //If not, ensure the Next button is enabled.
     document.getElementById("nextQuestion").disabled = false;
-
-  }
-
-}
-
-//Function to clear checkboxes.
-function clearCheckBoxes(){
-  let answers  = document.getElementsByClassName("answerOptions");
-  for (let i = 0; i < answers.length; i++) {
-    
-    if(answers[i].checked){
-      answers[i].checked = false;
-    }
-
   }
 }
+
 
 //Array to hold inventory results.
 let  resultsArray = [];
+
 //Function to collect current answers and place them in an array.
 function collectAnswers(inventoryResults){
 
@@ -243,21 +248,15 @@ function collectAnswers(inventoryResults){
 
     //Otherwise if this question has not been answered... 
     }else{
+
       //...push the selected answers from the first question to results with the question number and text.
       inventoryResults.push([currentQuestionNumber, answeredQuestion, questionAnswer]);
-
     }
-
   }
+  console.log(inventoryResults);
 }
 
-//Function to display question number / progress.
-function updateQuestionNumberDisplay(){
-  document.getElementById("questionNumberDisplay").innerHTML = currentQuestionNumber; //currentTest[index].split('|')[0];
-  document.getElementById("testLengthDisplay").innerHTML = Object.keys(currentTest).length;
-}
-
-//Function to move to next question, to be called onClick on Next button. //Event handlers to be moved out of HTML later.
+//Function to move to next question, to be called onclick of Next button.
 function moveToNextQuestion() {
 
   //Collect Answers and push to array.
@@ -273,34 +272,22 @@ function moveToNextQuestion() {
   clearCheckBoxes()
 }
 
+//Get Next button element.
+let nextButton = document.getElementById("nextQuestion");
 //Add event listeners for Next button.
-document.getElementById("nextQuestion").addEventListener("click", moveToNextQuestion); //Then populate the next question.
-
-//Function to move to next question, to be called onClick on Next button. //Event handlers to be moved out of HTML later.
-function moveToPreviousQuestion() {
-  
-  //Collect Answers and push to array.
-  collectAnswers(resultsArray);
-
-  //Subtract 1 from the current Question number.
-  currentQuestionNumber--;
-
-  //Re-populate current question with the next questions data.
-  populateQuestions(currentTest, currentQuestionNumber);
-
-  //Clear answers.
-  clearCheckBoxes()
-
-}
-
-//Add event listener for Previous button.
-document.getElementById("previousQuestion").addEventListener("click", moveToPreviousQuestion); //Then populate the next question.
+nextButton.addEventListener("click", moveToNextQuestion); //Then populate the next question.
 
 //Set the currentTest object to the result of the extract questions function. This creates the test object when the user loads the page compiling the questions and answers from hidden HTML elements provided by the CMS.
 let currentTest = extractQuestions();
 
 //Initialize the question index at question 1.
 let currentQuestionNumber = 1;
+
+// Create a URLSearchParams object based on the current URL to skip to a specific question using ?qn=N
+const urlSearchParams = new URLSearchParams(window.location.search);
+// Get the value of the "qn" parameter
+const qn = urlSearchParams.get('qn');
+let currentQuestionNumber = qn;
 
 //Start the test by populating the first question and answer sets.
 populateQuestions(currentTest, currentQuestionNumber);
